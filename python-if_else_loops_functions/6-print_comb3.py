4a07e3659d0815571c59ddb400dba8f1590fe045
Valentin <5619@holbertonstudents.com>
2023-01-17 07:47:16 -0800

    6. prints all possible different combinations

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for d in range(0, 10):
     for u in range(d + 1, 10):
-        print("{}{}".format(d, u), end=", " if d == 8 and u == 9 else "\n")
+        print("{}{}".format(d, u), end="\n" if d == 8 and u == 9 else ", ")
