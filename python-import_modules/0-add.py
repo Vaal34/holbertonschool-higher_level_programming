dba62090a69621fa166010b35ca4db7e1f4911f9
Valentin <5619@holbertonstudents.com>
2023-01-18 08:04:37 -0800

    0. import add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+# Ce bloc de code ne sera exécuté que lorsque le script est exécuté directement.
 if __name__ == "__main__":
-    import add_0 as add
+    # Importation du module add_0 avec le nom add
+    from add_0 import add
+    # déclaration des variables a, b avec leurs valeurs respectives
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add.add(a, b)))
+    # affiche le résultat de l'addition des deux variables avec l'utilisation de la fonction add du module add_0
+    print("{} + {} = {}".format(a, b, add(a, b)))
