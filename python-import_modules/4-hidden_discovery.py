5f2433ef37a7f757613bf1f8ffa22801f96f509d
Valentin <5619@holbertonstudents.com>
2023-01-19 07:34:01 -0800

    5. import and print a variable

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,12 @@
 if __name__ == "__main__":
     import statistics
     import hidden_4 as a
+    # On crée une variable "j" le module importé "a"
     j = dir(a)
     for i in range(len(j)):
+        # On vérifie si le premier caractère de chaque élément est _.
         if j[i][0] == '_':
+            # Si oui, l'élément est ignoré,
             continue
+        # Sinon il est affiché à l'écran.
         print("{}".format(j[i]))
