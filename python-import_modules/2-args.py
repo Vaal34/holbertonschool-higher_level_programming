7d8d34827517f9e287cd4727a3c4fda65f9e30d6
Valentin <5619@holbertonstudents.com>
2023-01-19 03:00:09 -0800

    2. args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
-import sys
+    j = 0
+    k = 0
+    for i in sys.argv[1:]:
+        j += 1
 
-j = 0
-k = 0
-for i in sys.argv[1:]:
-    j += 1
+    if j == 1:
+        print("1 argument:")
+    elif j == 0:
+        print("0 argument.")
+    else:
+        print("{} arguments:".format(j))
 
-if j == 1:
-    print("1 argument:")
-elif j == 0:
-    print("0 argument.")
-else:
-    print("{} arguments:".format(j))
-
-for i in sys.argv[1:]:
-    k += 1
-    print("{}: {}".format(k, i))
+    for i in sys.argv[1:]:
+        k += 1
+        print("{}: {}".format(k, i))
