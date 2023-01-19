79686a9a1538326234bb32139cc11a9027001635
Valentin <5619@holbertonstudents.com>
2023-01-19 03:00:43 -0800

    2. args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     if j == 1:
         print("1 argument:")
     elif j == 0:
-        print("0 argument.")
+        print("0 arguments.")
     else:
         print("{} arguments:".format(j))
 
