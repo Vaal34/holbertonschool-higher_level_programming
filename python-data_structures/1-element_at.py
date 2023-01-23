dcfa769be4bc29f8dc8d34afb0590ab6a550625a
Valentin <5619@holbertonstudents.com>
2023-01-23 02:10:30 -0800

    1. return the integer at the idx

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return
+        return None
     elif idx > len(my_list):
-        return
+        return None
     return my_list.pop(idx)
