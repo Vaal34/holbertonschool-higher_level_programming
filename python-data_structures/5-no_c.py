5bae969da4f31cc8fb1775cde4548d70fce4f6f9
Valentin <5619@holbertonstudents.com>
2023-01-23 05:30:31 -0800

    5. remove letters of a string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,9 +2,9 @@
 def no_c(my_string):
     new_list = list(my_string)
     for i in new_list:
-        if i is 'c':
+        if i == 'c':
             new_list.remove('c')
-        elif i is 'C':
+        elif i == 'C':
             new_list.remove('C')
     new_list = "".join(new_list)
     return new_list
