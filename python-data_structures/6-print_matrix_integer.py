cb58723c074b9a3f272f71d983fc5ffd90ce366c
Valentin <5619@holbertonstudents.com>
2023-01-23 06:31:55 -0800

    6. print matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+        for i in matrix:
+            print()
+            for j in range(len(i)):
+                print("{}".format(i[j]), end=' ')
+        print()
