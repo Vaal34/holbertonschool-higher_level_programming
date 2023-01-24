538e5cca39ca5f898649cf9ec38b3ef73717e039
Valentin <5619@holbertonstudents.com>
2023-01-24 02:53:28 -0800

    6. print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for vertical in matrix:
         x = 0
         for horizontal in vertical:
+            # x incrémente a chaque tour de bcl pour pas print un esp a la fin
             x += 1
             if x is len(vertical):
                 print("{:d}".format(horizontal), end="")
