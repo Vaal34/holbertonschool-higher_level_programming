9f44d3e9d6ff44f22277f93fe571afddccf554e9
Valentin <5619@holbertonstudents.com>
2023-01-25 07:02:43 -0800

    1. Search and replace in a list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
+    for i in range(len(my_list) - 1):
         if my_list[i] is search:
             my_list[i] = replace
     return my_list
