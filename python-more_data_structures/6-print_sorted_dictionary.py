7d84d523dba0df6d5ac0fb79dd7dd89f31ed2306
Valentin <5619@holbertonstudents.com>
2023-01-26 00:36:32 -0800

    6. print dictionnary

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    print("Number: {}".format(a_dictionary['Number']))
+    print("ids: {}".format(a_dictionary['ids']))
+    print("language: {}".format(a_dictionary['language']))
+    print("track: {}".format(a_dictionary['track']))
