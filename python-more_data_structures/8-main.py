842b253bc1e2ceff8a8c7c57f1758097e81ffbd4
Valentin <5619@holbertonstudents.com>
2023-01-26 07:08:43 -0800

    10. return the key with the upper value

--- python-more_data_structures/8-main.py
+++ python-more_data_structures/8-main.py
@@ -3,9 +3,15 @@ simple_delete = __import__('8-simple_delete').simple_delete
 print_sorted_dictionary = \
     __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-my_dict = { 'a': "a", 'b': "b" , 'c': "c", 'd': "d", 'e': "e" }
-key = 'f'
-new_dict = simple_delete(my_dict, key)
-print_sorted_dictionary(my_dict)
-print("xx")
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+new_dict = simple_delete(a_dictionary, 'track')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
+
+print("--")
+print("--")
+new_dict = simple_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
 print_sorted_dictionary(new_dict)
