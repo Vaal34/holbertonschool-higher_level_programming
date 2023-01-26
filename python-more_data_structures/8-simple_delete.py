e110d6350a0ba67ab645fe6ecdc34a2650128e13
Valentin <5619@holbertonstudents.com>
2023-01-26 02:09:36 -0800

    8. deletes a key in a dictionary

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key is not a_dictionary:
+    if key not in a_dictionary:
         return a_dictionary
-    del a_dictionary[key]
+    else:
+        del a_dictionary[key]
     return a_dictionary
