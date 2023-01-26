a9fb1448594e2d90b4045cc0ef208b6d36019f4e
Valentin <5619@holbertonstudents.com>
2023-01-26 01:11:32 -0800

    6.  prints a dictionary by ordered keys

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    print("Number: {}".format(a_dictionary['Number']))
-    print("ids: {}".format(a_dictionary['ids']))
-    print("language: {}".format(a_dictionary['language']))
-    print("track: {}".format(a_dictionary['track']))
+    for i in sorted(a_dictionary):
+        print("{}: {}".format(i, a_dictionary[i]))
