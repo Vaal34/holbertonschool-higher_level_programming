0aeef3380f9b883b8bdbc2d7f0b887a9ec138ef8
Valentin <5619@holbertonstudents.com>
2023-01-27 01:02:29 -0800

    0. prints x elements of a list (try, except)

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        number_print = 0
+        i = 0
+        while i != x:
+            print(f"{my_list[i]}", end='')
+            i += 1
+            number_print += 1
+        print()
+        return x
+    except IndexError:
+        print()
+        return x
