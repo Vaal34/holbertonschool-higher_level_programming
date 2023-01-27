97cbf53bcd4ab5e424346661df771ad07d23a23f
Valentin <5619@holbertonstudents.com>
2023-01-27 01:46:00 -0800

    1. prints an integer expect print a string

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError:
-        print("{}".format(value))
+    except ValueError:
         return False
