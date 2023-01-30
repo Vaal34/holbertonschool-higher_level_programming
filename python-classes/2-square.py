b6050cfcde423df57372cde36b6a591a102fb60e
Valentin <5619@holbertonstudents.com>
2023-01-30 06:21:06 -0800

    2. task 1 + except and try

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,10 +6,9 @@ class Square:
     """ empty class Square that defines a square """
 
     def __init__(self, size=0):
-        """Private instance attribute"""
-        try:
-            self.__size = size
-            if size < 0:
-                raise ValueError("size must be >= 0")
-        except TypeError:
-            print("size must be an integer")
+        """Private instance attribute""" 
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
