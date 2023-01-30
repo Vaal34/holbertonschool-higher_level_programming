9907ac5f6191382811d13acb7d328c962af3164a
Valentin <5619@holbertonstudents.com>
2023-01-30 04:24:02 -0800

    1. Private instance attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" empty class Square that defines a square """
+
+
+class Square:
+    """ empty class Square that defines a square """
+
+    def __init__(self, __size):
+        """Private instance attribute"""
+        self.__size = __size
