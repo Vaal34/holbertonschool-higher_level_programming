9e20d27f3ad49628d42437c3dadb370f1b49835c
Valentin <5619@holbertonstudents.com>
2023-01-31 01:47:40 -0800

    6. print square with position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -32,11 +32,11 @@ class Square:
     @position.setter
     def position(self, value):
         """setter instance"""
-        if position[0] < 0 or position[1] < 0:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(position[0]) is not int or type(position[1]) is not int:
+        if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(position) is not tuple or len(position) < 2:
+        if type(value) is not tuple or len(value) < 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
