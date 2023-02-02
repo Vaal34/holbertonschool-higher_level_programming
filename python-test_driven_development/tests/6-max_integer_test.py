a1344bcdcfa2af7a338647f17ef8febcaef32d71
Valentin <5619@holbertonstudents.com>
2023-02-02 07:26:39 -0800

    5. add doctest.py

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -16,10 +16,10 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([3, 4, 2]), 4)
     
     def test_one_negatif_number(self):
-        self.assertEqual(max_integer([-4, 3, 1, 2]), -4)
+        self.assertEqual(max_integer([-4, 3, 1, 2]), 3)
     
     def test_only_negatif_number(self):
-        self.assertEqual(max_integer([-4, -3, 1, 2]), [-4, -3])
+        self.assertEqual(max_integer([-4, -3, -1, -2]), -1)
 
     def list_of_one_element(self):
         self.assertEqual(max_integer([2]), 2)
