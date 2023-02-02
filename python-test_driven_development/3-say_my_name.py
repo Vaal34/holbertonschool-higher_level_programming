81d1ab9d8f06141626be5c3665c2916f983353e2
Valentin <5619@holbertonstudents.com>
2023-02-02 02:33:51 -0800

    2. fix doctest with traceback and add doc in .py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Shebang """
+
+
 def say_my_name(first_name, last_name=""):
+    """ prints My name is <first name> <last name>"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     elif type(last_name) is not str:
