7128c3e426bea2c3be3180c340137f70442370f9
Valentin <5619@holbertonstudents.com>
2023-02-02 01:29:13 -0800

    1. function that divides all elements of a matrix.

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,6 +4,7 @@
 
 def matrix_divided(matrix, div):
     """ divides all elements of a matrix """
+    error = ("matrix must be a matrix (list of lists) of integers/floats")
     ref_size = len(matrix[0])
     matrix_matrix = []
     for i in matrix:
@@ -12,7 +13,7 @@ def matrix_divided(matrix, div):
             if type(div) not in [int, float]:
                 raise TypeError("div must be a number")
             elif type(j) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(error)
             valeur_matrix.append(round(float(j / div), 2))
         if ref_size != len(i):
             raise TypeError("Each row of the matrix must have the same size")
