af9a8dbccaa6becd9c84084f7820487c1c592197
Valentin <5619@holbertonstudents.com>
2023-02-02 04:35:55 -0800

    3. print sqaure and build doctest

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Doc """
+
+
 def print_square(size):
+    """ Print Square """
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
