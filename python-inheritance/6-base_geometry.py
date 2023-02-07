c8e91b1c80c5d94517b43a8d791e423aba074e23
Valentin <5619@holbertonstudents.com>
2023-02-07 01:57:03 -0800

    6. add public instance area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Doc """
+
+
+class BaseGeometry:
+    """ Class BaseGeometry """
+
+    def area(self):
+        """ area """
+        raise Exception("area() is not implemented")
