d2935a65555cd96f11b3c24c83df82628778c78f
Valentin <5619@holbertonstudents.com>
2023-02-07 06:59:04 -0800

    8. create subclass width height

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -18,12 +18,12 @@ class BaseGeometry:
             raise ValueError(f"{name} must be greater than 0")
         
 
-class Rectangle(BaseException):
+class Rectangle(BaseGeometry):
     """ SubClass BaseGeometry """
     
     def __init__(self, width, height):
         """ Instantiation """
         self.__height = height
         self.__width = width
-        self.__width = BaseGeometry.integer_validator(self, "width", width)
-        self.__height = BaseGeometry.integer_validator(self, "height", height)
+        self.__width == BaseGeometry.integer_validator(self, "width", width)
+        self.__height == BaseGeometry.integer_validator(self, "height", height)
