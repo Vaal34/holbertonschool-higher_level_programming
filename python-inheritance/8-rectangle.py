1debdb9f2679c0c22d8e488f3c92e810e7c3a148
Valentin <5619@holbertonstudents.com>
2023-02-08 00:59:01 -0800

    8. create subclass width height

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,9 +3,11 @@
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """ SubClass BaseGeometry """
-    
+
     def __init__(self, width, height):
         """ Instantiation """
         self.__height = height
