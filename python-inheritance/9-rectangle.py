c0ee8b8c2b202ac4d577479bbb289c22bc308678
Valentin <5619@holbertonstudents.com>
2023-02-08 01:27:28 -0800

    9. implement area method and __str__

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Doc """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ SubClass BaseGeometry """
+
+    def __init__(self, width, height):
+        """ Instantiation """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ area """
+        return self.__height * self.__width 
+    
+    def __str__(self):
+        return f"[Rectangle] {self.__height}/{self.__width}"
