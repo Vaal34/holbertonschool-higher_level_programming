dff8eb1cc155cccee9505ca882ca71cfe7ac3a8f
Valentin <5619@holbertonstudents.com>
2023-02-08 05:22:39 -0800

    1. write in a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,8 @@
 
 def write_file(filename="", text=""):
     """ write et on open a file """
-    with open(filename, 'w') as w:
+    with open(filename, 'w', encoding="utf-8") as w:
         w.write(text)
+
+    w.close
+    return len(text)
