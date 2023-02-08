72a227b8627afeb07f7abe0f1e7fe938699b53df
Valentin <5619@holbertonstudents.com>
2023-02-08 07:34:04 -0800

    7. script that adds arguments to a list, in a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Doc """
-import json, sys
+import json
+import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
