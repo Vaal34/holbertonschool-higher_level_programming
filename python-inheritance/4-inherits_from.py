a91e2543bc4c5788a67bdf09fb5bf1cc65921ef4
Valentin <5619@holbertonstudents.com>
2023-02-08 01:48:52 -0800

    11. replace rectangle by Square

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,12 +7,3 @@ def inherits_from(obj, a_class):
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     return False
-
-
-"""
-Si "isinstance(obj, a_class)" renvoie "True", cela signifie que "obj"
-est une instance de "a_class" ou d'une de ses sous-classes.
-Et si "type(obj) is not a_class" renvoie également
-"True", cela signifie que "obj" n'est pas un objet
-de la classe "a_class" elle-même, mais plutôt une sous-classe de "a_class".
-"""
