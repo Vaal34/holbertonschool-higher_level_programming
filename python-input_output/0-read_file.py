a2ebdbc2556ae58f13f7fd0168d25cb9bfadbfdc
Valentin <5619@holbertonstudents.com>
2023-02-08 05:03:32 -0800

    0. read the file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 """ Doc """
 
 def read_file(filename=""):
+    """ open and read file """
     with open('my_file_0.txt', 'r') as text:
         for value in text:
             print(value, end='')
