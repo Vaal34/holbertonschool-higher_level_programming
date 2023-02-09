b370129daaf1d8e7d7bc42cf110d1726d4fd3ed3
Valentin <5619@holbertonstudents.com>
2023-02-09 06:50:15 -0800

    10.

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" My class module """
+
+
+class Student:
+    """ Class Student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialisation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ class to json """
+        if type(attrs) is list:
+            return attrs
+        else:
+            return self.__dict__
