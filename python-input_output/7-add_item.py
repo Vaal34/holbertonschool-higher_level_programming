0bef69259cbf8d215bed7b6c09222d045742042d
Valentin <5619@holbertonstudents.com>
2023-02-09 00:36:42 -0800

    7. script that adds arguments to a list, in a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,11 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-list = []
+
+try:
+    list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    list = []
 for i in sys.argv[1:]:
     list.append(i)
 save_to_json_file(list, 'add_item.json')
-load_from_json_file('add_item.json')
