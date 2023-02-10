ebb9ae79f3a9ffc33f7d36d9aff1dd256edd6d2c
Valentin <5619@holbertonstudents.com>
2023-02-10 06:24:39 -0800

    10. return atttributs of class in dict format

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -11,18 +11,18 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-def to_json(self, attrs=None):
-    """ Convertir la classe en JSON """
-    # Créer un dictionnaire vide
-    dico = {}
-    # Parcourir les attributs de la classe
-    for i in self.__dict__:
-        # Si les attributs spécifiques ont été fournis
-        if attrs is not None:
-            # Si l'attribut actuel est dans la liste des attributs spécifiques
-            if i in attrs:
-                # Ajouter l'attribut et sa valeur au dictionnaire
-                dico[i] = self.__dict__[i]
-                return dico
-    # Retourner tous les attributs de la classe sous forme de dictionnaire
-    return self.__dict__
+    def to_json(self, attrs=None):
+        """ Convertir la classe en JSON """
+        # Créer un dictionnaire vide
+        dico = {}
+        # Parcourir les attributs de la classe
+        for i in self.__dict__:
+            # Si les attributs spécifiques ont été fournis
+            if attrs is not None:
+                # Si l'attribut actuel est dans la liste des attributs spécifiques
+                if i in attrs:
+                    # Ajouter l'attribut et sa valeur au dictionnaire
+                    dico[i] = self.__dict__[i]
+                    return dico
+        # Retourner tous les attributs de la classe sous forme de dictionnaire
+        return self.__dict__
