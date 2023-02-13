cede141579d52bc4922052bf762bcef368630ebf
Valentin <5619@holbertonstudents.com>
2023-02-13 02:20:05 -0800

    1. create the class Base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Doc """
+
+class Base:
+    """ Class Base """
+    
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initialisation """
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
