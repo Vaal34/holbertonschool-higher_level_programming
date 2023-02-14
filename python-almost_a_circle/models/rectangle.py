b03e85ed2d877e2d17dc96127101766dd9cad366
Valentin <5619@holbertonstudents.com>
2023-02-14 06:24:00 -0800

    9. add kwargs in update method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,20 +79,25 @@ class Rectangle(Base):
         string2 = f" - {self.width}/{self.height}"
         return string + string2
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ update func"""
-        if args:
-            # si un args exist id prend a la valeur de args[0]
-            self.id = args[0]
-        if len(args) > 1:
-            # si le 2eme args exist width prend a la valeur de args[1]
-            self.__width = args[1]
-        if len(args) > 2:
-            # si 3eme args exist height prend a la valeur de args[2]
-            self.__height = args[2]
-        if len(args) > 3:
-            # si 4eme args exist x prend a la valeur de args[3]
-            self.__x = args[3]
-        if len(args) > 4:
-            # si 5eme args exist y prend a la valeur de args[4]
-            self.__y = args[4]
+
+        if len(args) > 0:    
+            if args:
+                # si un args exist id prend a la valeur de args[0]
+                self.id = args[0]
+            if len(args) > 1:
+                # si le 2eme args exist width prend a la valeur de args[1]
+                self.__width = args[1]
+            if len(args) > 2:
+                # si 3eme args exist height prend a la valeur de args[2]
+                self.__height = args[2]
+            if len(args) > 3:
+                # si 4eme args exist x prend a la valeur de args[3]
+                self.__x = args[3]
+            if len(args) > 4:
+                # si 5eme args exist y prend a la valeur de args[4]
+                self.__y = args[4]
+        else:
+            for cle, valeur in kwargs.items():
+                setattr(self, cle, valeur)
