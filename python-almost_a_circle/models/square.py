33aa632441edeaa51530f28531ad17363a30c6e0
Valentin <5619@holbertonstudents.com>
2023-02-15 05:58:20 -0800

    12. args kwargs

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,7 +12,30 @@ class Square(Rectangle):
         """ initialisation """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
+
     def __str__(self):
         string = f"[Square] ({self.id}) {self.x}/{self.y}"
         string2 = f" - {self.height}"
         return string + string2
+
+    def update(self, *args, **kwargs):
+        if len(args) > 0:
+            if args:
+                self.id = args[0]
+            elif len(args) > 2:
+                self.size = args[1]
+            elif len(args) > 3:
+                self.x = args[2]
+            elif len(args) > 4:
+                self.y = args[3]
+        else:
+            for cle, valeur in kwargs.items():
+                setattr(self, cle, valeur)        
