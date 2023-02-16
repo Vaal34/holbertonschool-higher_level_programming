2b509e0eff8e9345f6581199d967188d87e8605f
Valentin <5619@holbertonstudents.com>
2023-02-16 00:43:25 -0800

    15. if list is none return []

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 
 import json
 
+
 class Base:
     """ Class Base """
 
@@ -18,4 +19,6 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return []
         return json.dumps(list_dictionaries)
