0d21f6a50ab1588548542902f6efcbc515beb141
Valentin <5619@holbertonstudents.com>
2023-03-14 07:59:26 -0700

    1.  create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- script that creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'root' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
