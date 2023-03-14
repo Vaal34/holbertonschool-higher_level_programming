a3e6b8d7b90f18ebfe2dcb65f10504e9dedbbfce
Valentin <5619@holbertonstudents.com>
2023-03-14 02:34:45 -0700

    10. fixed by select name, score

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT * FROM second_table ORDER BY score DESC;
+SELECT name, score FROM second_table ORDER BY score DESC;
