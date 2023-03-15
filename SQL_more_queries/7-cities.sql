16848bcfcf37cd7f6371012838811df2ccea6d8d
Valentin <5619@holbertonstudents.com>
2023-03-14 17:31:26 -0700

    7. test fix

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    state_id INT NOT NULL FOREIGN KEY (id) REFERENCES states (id),
+    state_id INT NOT NULL FOREIGN KEY (id) REFERENCES hbtn_0d_usa.states (id),
     name VARCHAR(256) NOT NULL
 );
