16ebda68de904cbda43bee69961d13acb8da7e50
Valentin <5619@holbertonstudents.com>
2023-03-16 03:22:47 -0700

    14. fix where with dexter and not id

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,5 @@ SELECT
 FROM
     tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
-WHERE tv_show_genres.show_id = 8;
+INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter';
