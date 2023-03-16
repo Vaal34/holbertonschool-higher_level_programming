8fc571480018e92cf0eefa49f0c9579862788828
Valentin <5619@holbertonstudents.com>
2023-03-15 18:27:16 -0700

    13. add commentaires

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,11 @@
 -- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
 
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+-- Count permet de compter le nombre de fois que show_id est dans les genre_id
 FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC;
+-- Join pour afficher que les genres pour lesquels il y a au moins une série associée dans la table tv_show_genres
+GROUP BY tv_show_genres.genre_id 
+-- regroupe les genre_id de la table tv_show_genres.
+-- GROUP BY est nécessaire pour diviser les données en groupes et appliquer la fonction COUNT à chaque groupe individuellement.
+ORDER BY number_of_shows DESC;  
