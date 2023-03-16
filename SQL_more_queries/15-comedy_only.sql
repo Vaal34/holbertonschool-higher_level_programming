80d0a5ff2b3ce2f066d3a6e467870dfa6c8aee9c
Valentin <5619@holbertonstudents.com>
2023-03-16 04:37:38 -0700

    15. lists all Comedy shows

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Active: 1678888257555@@127.0.0.1@3306@hbtn_0d_tvshows
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT 
+    tv_shows.title
+FROM
+    tv_shows
+INNER JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
+INNER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
+WHERE tv_genres.name = 'Comedy';
