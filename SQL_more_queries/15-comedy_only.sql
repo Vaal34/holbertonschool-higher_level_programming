aabb6798a5cc8ced3c71d71def74de62ca01ec36
Valentin <5619@holbertonstudents.com>
2023-03-16 14:51:00 -0700

    15. order by ASC

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -6,4 +6,5 @@ FROM
     tv_shows
 INNER JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
 INNER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
-WHERE tv_genres.name = 'Comedy';
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
