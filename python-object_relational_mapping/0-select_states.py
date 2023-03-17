2eecae1996db310d27b4169cf1a5e77ea4ea9f23
Valentin <5619@holbertonstudents.com>
2023-03-16 18:12:21 -0700

    0. why dosen't fuunct

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,28 +8,29 @@ import MySQLdb    # importe la bibliothèque MySQLdb
 # établit une connexion à la base de données MySQL en utilisant
 # les arguments passés au script
 # (nom d'utilisateur, mot de passe, nom de la base de données)
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3]
-)
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
-# crée un objet curseur pour interagir avec la base de données
-cur = db.cursor()
+    # crée un objet curseur pour interagir avec la base de données
+    cur = db.cursor()
 
-# exécute une requête SQL pour sélectionner
-# toutes les lignes de la table "states" et les trier par ID
-cur.execute("SELECT * FROM states ORDER BY states.id")
+    # exécute une requête SQL pour sélectionner
+    # toutes les lignes de la table "states" et les trier par ID
+    cur.execute("SELECT * FROM states ORDER BY states.id")
 
-# récupère les résultats de la requête sous forme de liste de tuples
+    # récupère les résultats de la requête sous forme de liste de tuples
 
-rows = cur.fetchall()
+    rows = cur.fetchall()
 
-for row in rows:
-    print(row)
-# ferme le curseur et la connexion à la base de données
+    for row in rows:
+        print(row)
+    # ferme le curseur et la connexion à la base de données
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
