bb8e2b4019a46dfc3eccea1ce5e5d64bc459c44f
Valentin <5619@holbertonstudents.com>
2023-04-09 14:37:55 -0700

    1. print many phrase

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const phrase = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; phrase[i]; i++) { console.log(phrase[i]); } // Affiche la valeur de `myVar` dans la console
+
+/* peut être simplifié par :
+    for (let value of phrase) {
+        console.log(value);}
+} */
