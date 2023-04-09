ca98b648dcdbc7efca3ac314656b311f445f0d78
Valentin <5619@holbertonstudents.com>
2023-04-09 14:22:22 -0700

    0. semi marted fix

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const myVar = "JavaScript is amazing"
-console.log(myVar) // Affiche la valeur de `myVar` dans la console
+const myVar = 'JavaScript is amazing';
+console.log(myVar); // Affiche la valeur de `myVar` dans la console
