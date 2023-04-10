b0d15c214dff6f7bf85e8be4276485cf239f5609
Valentin <5619@holbertonstudents.com>
2023-04-10 05:05:14 -0700

    10 semistandard fix

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,17 @@
 #!/usr/bin/node
 
 const argv = process.argv;
-let num = Number(argv[2]);
+const num = Number(argv[2]);
 
-function facto(num) {
-  if (num <= 1) {
+function facto (num) {
+  if (isNaN(num)) {
+    return 1;
+  } else if (num <= 1) {
+    return 1;
+  } else if (num === undefined) {
     return 1;
   }
   return num * facto(num - 1);
 }
 
-console.log(facto(num))
+console.log(facto(num));
