2b7c50e8b6788c28648bd5fe02c4fd6be808e080
Valentin <5619@holbertonstudents.com>
2023-04-10 05:49:00 -0700

    11. update sort

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -7,10 +7,9 @@ const list = [];
 if (argv.length <= 3) { // if 0 or just 1 args
   console.log(0);
 } else {
-  for (let i = 2; i <= argv.length; i++) {
-    list.push(argv[i]); // append the list
+  for (let i = 2; i < argv.length; i++) {
+    list.push(Number(argv[i])); // append the list
   }
-  list.sort(); // sort lower ... upper
-  list.reverse(); // reverse upper .. lower
+  list.sort((lower, upper) => upper - lower); // trier la liste dans l'ordre décroissant
   console.log(list[1]); // return 2nd value
 }
