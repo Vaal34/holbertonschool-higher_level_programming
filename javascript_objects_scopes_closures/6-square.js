9e9ff2956bf763052151a559396fc006dced3c72
Valentin <5619@holbertonstudents.com>
2023-04-10 09:06:47 -0700

    6. replace X by C if exist in variable c

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle { // crée un enfant de la classe Rectangle
+  constructor (size) {
+    super(size, size);
+    // Appelle le constructeur de la classe parent en lui passant la taille du carré pour la longueur et la largeur
+  }
+
+  charPrint (c) {
+    if (c === undefined) { // si c est vide alors c vaut 'X'
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) { // boucle hauteur
+      for (let j = 0; j < this.width; j++) { // boucle largeur
+        process.stdout.write(c); // print X sans sauté de ligne
+      }
+      console.log();
+    }
+  }
+}
+
+module.exports = Square;
