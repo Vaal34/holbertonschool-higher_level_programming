28ee62a0b37de74f20d6edaf89a39d329395cb5f
Valentin <5619@holbertonstudents.com>
2023-04-10 07:56:50 -0700

    2. undefined if error exist

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) { // Les nouveaux rectangle predrons c'est arguments lors de leur creation
+    this.width = w;
+    this.height = h;
+    if (h <= 0 || w <= 0 || h === undefined || w === undefined) {
+      this.height = undefined;
+      this.width = undefined;
+    }
+  }
+}
+module.exports = Rectangle; // permet de pouvoir utiliser la classe dans d'autres fichiers
