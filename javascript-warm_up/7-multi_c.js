fdf270b2be6b236880d4f0b94ade9831d5fc5037
Valentin <5619@holbertonstudents.com>
2023-04-10 02:25:14 -0700

    7. loop x fois

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const x = process.argv[2];
+
+if (x === undefined) {
+  console.log('Missing number of occurrences');
+} else if (x < 0) {
+  // pass // use '// pass' for do nothing
+} else {
+  for (let i = 0; i !== x; i++) {
+    console.log('C is Fun');
+  }
+}
