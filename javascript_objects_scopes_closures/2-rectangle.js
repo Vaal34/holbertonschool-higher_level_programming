bcece89ae54263ad146669cb75fe338a2589635a
Valentin <5619@holbertonstudents.com>
2023-04-10 08:25:46 -0700

    2. return empty if error

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) { // Les nouveaux rectangle predrons c'est arguments lors de leur creation
+    if (h <= 0 || w <= 0 || h === undefined || w === undefined) { // si un param est < 0 ou n'st pas donné
+      return;
+    }
     this.width = w;
     this.height = h;
-    if (h <= 0 || w <= 0 || h === undefined || w === undefined) {
-      return {}
-    }
   }
 }
 module.exports = Rectangle; // permet de pouvoir utiliser la classe dans d'autres fichiers
