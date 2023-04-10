03037febd95bb90cc6c34444a6df54bdd5823811
Valentin <5619@holbertonstudents.com>
2023-04-10 02:30:15 -0700

    7. why semismarted dont accept != ... !== create loop infiny

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -7,7 +7,7 @@ if (x === undefined) {
 } else if (x < 0) {
   // pass // use '// pass' for do nothing
 } else {
-  for (let i = 0; i !== x; i++) {
+  for (let i = 0; i < x; i++) {
     console.log('C is Fun');
   }
 }
