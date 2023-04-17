b01ae56fefcfd98b85dba088844662fc9c45855b
Valentin <5619@holbertonstudents.com>
2023-04-17 06:32:43 -0700

    semistandart

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
-const argv = process.argv
+const argv = process.argv;
 const fs = require('fs');
 const filename = argv[3];
-const request = require('request'); 
+const request = require('request');
 
 request(argv[2], function (err, content, body) {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
-    const loremIpsum = body
+    const loremIpsum = body;
     fs.writeFile(filename, loremIpsum, err => {
-        if (err) { console.log(err); }
+      if (err) { console.log(err); }
     });
   }
-})
+});
