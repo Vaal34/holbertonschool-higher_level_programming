b2a15fa67a00e7ba5dcdb725248bd72dbb7fd9b6
Valentin <5619@holbertonstudents.com>
2023-04-17 04:12:55 -0700

    semismarted fix + farm point

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,11 +2,11 @@
 
 const argv = process.argv;
 const request = require('request');
-url = `https://swapi-api.hbtn.io/api/films/${argv[2]}`
+const url = `https://swapi-api.hbtn.io/api/films/${argv[2]}`;
 
-request(url, function(err, data, body) {
-    if (err) throw err;
-    else {
-        console.log(JSON.parse(body).title);
-    }
+request(url, function (err, data, body) {
+  if (err) throw err;
+  else {
+    console.log(JSON.parse(body).title);
+  }
 });
