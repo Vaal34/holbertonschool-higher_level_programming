d6261fa98a2859b7c793691b2624df1aa6c33a5a
Valentin <5619@holbertonstudents.com>
2023-04-18 02:06:59 -0700

    count the number of task completed for each id

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
-const argv = process.argv
-const url = argv[2]
-let dict = {}
+const argv = process.argv;
+const url = argv[2];
+const dict = {};
 
 request(url, function (err, content, body) {
-    let count = 0
-    if (err) {
-        console.log(err)
-    } else {
-        let data = JSON.parse(body)
-        
-            for (value of data) {
-                if (value["completed"] == true)
-                    count += 1
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    const maxLength = data.length - 1;
+    const numberOfId = data[maxLength];
+    for (let i = 1; i <= numberOfId.userId; i++) {
+      let count = 0;
+      for (const value of data) {
+        if (value.userId === i) {
+          if (value.completed === true) { count += 1; }
         }
+      }
+      dict[i] = count;
     }
-    console.log(count)
-})
+    console.log(dict);
+  }
+});
